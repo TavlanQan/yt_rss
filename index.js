f8113cbecbbb2f18d63636f1c9a974fe69aebb04
QTavlan <devalanart@gmail.com>
2026-08-16 09:44:17 +0200

    helpers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5005;
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const CACHE_TTL_MS = parseInt(process.env.CACHE_TTL_MS) || 5 * 60 * 1000; // 5 минут
 
